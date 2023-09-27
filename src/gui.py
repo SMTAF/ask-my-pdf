2c28fdaeb88b3d717a1576881576ca246761e365
SMTAF <53369282+SMTAF@users.noreply.github.com>
2023-09-27 13:13:51 +0300

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -160,7 +160,7 @@ def ui_temperature():
 
 def ui_fragments():
 	#st.number_input('fragment size', 0,2000,200, step=100, key='frag_size')
-	st.selectbox('fragment size (characters)', [0,200,300,400,500,600,700,800,900,1000], index=3, key='frag_size')
+	st.selectbox('fragment size (characters)', [0,200,300,400,500,600,700,800,900,1000], index=9, key='frag_size')
 	b_reindex()
 	st.number_input('max fragments', 1, 10, 4, key='max_frags')
 	st.number_input('fragments before', 0, 3, 1, key='n_frag_before') # TODO: pass to model
